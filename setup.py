8867f061740d91faaa48d12bf7181a4e63d158a9
Michael Welles <mlwelles@gmail.com>
2018-08-07 12:21:01 -0400

    removing dist

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+# Terrible hack, but not sure how else to make sure cython is installed
+# prior to the requirement beeing needed, since it install_requires and
+# setup_requires can't be now be parsed to specify the dependency prior to
+# it being used
+try:
+    from Cython.Build import cythonize
+except:
+    import subprocess
+    subprocess.run(('pip3', 'install', 'cython', 'numpy'))
 import numpy
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 from setuptools import setup, Extension
-
 from sparse_dot_topn import __version__
 
 ext_utils = Extension('sparse_dot_topn.sparse_dot_topn',
@@ -18,10 +26,11 @@ setup(name='sparse_dot_topn',
       setup_requires=[
           # Setuptools 18.0 properly handles Cython extensions.
           'setuptools>=18.0',
-          'Cython',
+          'cython',
       ],
        install_requires=[
-          'Cython',
+           'numpy',
+           'cython',
       ],
       packages=['sparse_dot_topn'],
       cmdclass={'build_ext': build_ext},
